600eb7d2465ef66e2427c7550860ceceeeb958a7
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-09-12 00:04:57 +0900

    基底クラス作成

---
+++ Assets/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Stateのベースクラス
+/// 継承して使う
+/// </summary>
+public class State
+{
+    public StateContext Context { get; private set; }
+
+    public Action OnEnter;
+    public Action OnExit;
+
+    public State(StateContext context)
+    {
+        Context = context;
+    }
+}
---
+++ Assets/StateContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+public class StateContext
+{
+    public List<State> StateList = new List<State>();
+    public State CurrentState { get; private set; }
+
+    //  public bool SelfTransit = true;
+
+    object Locker = new object();
+
+    public void setCurrentState(State state)
+    {
+        if (state == null || !StateList.Contains(state))
+            return;
+
+        CurrentState = state;
+    }
+
+    public void addState(State state)
+    {
+        if (state == null || StateList.Contains(state))
+            return;
+
+        StateList.Add(state);
+    }
+
+    public void transitState(State targetState)
+    {
+        if (targetState == null)
+            return;
+
+        lock (Locker)
+        {
+            CurrentState?.OnExit();
+            CurrentState = targetState;
+            CurrentState?.OnEnter();
+        }
+    }
+
+}
