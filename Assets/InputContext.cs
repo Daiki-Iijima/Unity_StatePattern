77ba9d754abb7337243cb696ac5fefba4e470ed3
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-09-12 23:30:46 +0900

    Input用のStateを作成

---
+++ Assets/InputContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputContext : StateContext
+{
+    public void onInput()
+    {
+        (CurrentState as InputState).InputEvent();
+    }
+}
---
+++ Assets/InputState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputState : State
+{
+    public Action InputEvent;
+
+    public InputState(StateContext context) : base(context)
+    {
+        Context.addState(this);
+    }
+}
